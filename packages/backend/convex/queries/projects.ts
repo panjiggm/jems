fc347e5c6200c495f053f7172a2be27112fd7e53
panjiggm <panjiggm@gmail.com>
2025-09-24 20:58:35 +0700

    fixing list query

--- packages/backend/convex/queries/projects.ts
+++ packages/backend/convex/queries/projects.ts
@@ -5,13 +5,12 @@ import { paginationOptsValidator } from "convex/server";
 
 export const list = query({
   args: {
-    search: v.optional(v.string()),
     paginationOpts: paginationOptsValidator,
   },
   handler: async (ctx, args) => {
     const userId = await getUserId(ctx);
 
-    // Get all projects for the user
+    // Get all projects for the user with pagination
     const projects = await ctx.db
       .query("projects")
       .withIndex("by_user_createdAt", (q) => q.eq("userId", userId!))
