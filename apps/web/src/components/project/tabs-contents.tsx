0d00f232243b8e8b2aacecacd8a00bcff8327183
panjiggm <panjiggm@gmail.com>
2025-09-25 21:47:33 +0700

    fix type projectId error

--- apps/web/src/components/project/tabs-contents.tsx
+++ apps/web/src/components/project/tabs-contents.tsx
@@ -2,6 +2,7 @@
 
 import { Tabs, TabsList, TabsTrigger, TabsContent } from "@/components/ui/tabs";
 import { Kanban, Table, List } from "lucide-react";
+import { Id } from "@packages/backend/convex/_generated/dataModel";
 import KanbanView from "./views/kanban-view";
 import TableView from "./views/table-view";
 import ListView from "./views/list-view";
@@ -10,7 +11,7 @@ interface TabsContentsProps {
   className?: string;
   activeTab: string;
   onTabChange: (value: string) => void;
-  projectId?: string;
+  projectId?: Id<"projects">;
   userId?: string;
 }
 
